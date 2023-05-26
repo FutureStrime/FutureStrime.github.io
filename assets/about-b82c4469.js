980cfb72ed339371902ffe3d1bd52c52ac8d5be2
FutureStrime <ljx091117@outlook.com>
2023-05-26 19:43:52 +0800

    Fixed BUG

--- assets/about-b82c4469.js
+++ assets/about-b82c4469.js
@@ -1,1 +1,1 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{l as e,t as o}from"./index-23bf0e44.js";const c={},r={style:{"text-align":"center"}};function n(s,_){return e(),o("p",r,"ABOUT PAGE")}const l=t(c,[["render",n]]);export{l as default};
+import{_ as t}from"./plugin-vue_export-helper-c27b6911.js";import{l as e,t as o}from"./index-23bf0e44.js";const c={},r={style:{"text-align":"center"}};function n(s,_){return e(),o("p",r,"ABOUT PAGE")}const l=t(c,[["render",n]]);export{l as default};
